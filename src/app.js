00927a4a9fc3cf92baf26f8252f520fab4372050
IdeaProjects <harshitchourasiya0808@gmail.com>
2024-07-13 01:40:02 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    Credential: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export {app}
+
+// URL se jab bhi koi data ata hai vah req.params se ata hai.
+
+// req.body me differnt different form means (json) me data ata hai
