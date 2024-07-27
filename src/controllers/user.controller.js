a01eafb749ff139f8212b5439dfea8d57555d901
IdeaProjects <harshitchourasiya0808@gmail.com>
2024-07-27 15:16:08 +0530

    add getWatchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse} from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
 import e from "express";
+import mongoose from "mongoose";
 
 const generateAccessAndRefereshTokens = async(userId) => {
     try {
@@ -338,6 +339,132 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(200, user, "Cover image updated successfully")
 })
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const {username} = req.params
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+                $lookup: {
+                    from: "subscriptions",
+                    localField: "_id",
+                    foreignField: "channel",
+                    as: "subscribers"
+                }
+        },
+         {
+                $lookup: {
+                    from: "subscriptions",
+                    localField: "_id",
+                    foreignField: "subscriber",
+                    as: "subscribedTo"
+                    }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channesSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                    isSubscribed: {
+                        $cond: {
+                            if: {$in: [req.user?._id, "subscribers.subscriber"]},
+                            then: true,
+                            else: false
+                        }
+                    }
+            }
+            
+        },
+        {
+             $project: {
+                            fullName: 1,
+                            username: 1,
+                            subscribersCount: 1,
+                            channesSubscribedToCount: 1,
+                            isSubscribed: 1,
+                            avatar: 1,
+                            coverImage: 1,
+                            email: 1
+                        }
+        }
+        
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404, "channel does not exists")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0], "user channel fatched successfully")
+    )
+})
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: videos,
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                       fullName: 1,
+                                       username: 1,
+                                       avatar: 1,
+
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+    ])
+
+    return res
+    .status(200)
+    .json(
+        200,
+        user[0].watchHistory,
+        "watch History fatched successfully"
+    )
+})
+
 export {
      registerUser,
      loginUser,
@@ -347,5 +474,7 @@ export {
      getCurrentUser,
      updateAccountDetails,
      updateUserAvatar, 
-     updateUserCoverImage
+     updateUserCoverImage,
+     getUserChannelProfile,
+     getWatchHistory 
      }
